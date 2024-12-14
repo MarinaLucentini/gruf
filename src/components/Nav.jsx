8147b26ca9a29c351c304b480a156f77bc3a3c0e
MARINA MARIA LUCENTINI <158228930+MarinaLucentini@users.noreply.github.com>
2024-12-14 17:15:27 +0100

    add logo

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,13 @@
+import logo from "../assets/logo.png";
+
 const Nav = () => {
-  return <></>;
+  return (
+    <>
+      <div className="flex align-center justify-between">
+        <img src={logo} alt="logo" />
+        <p>Welcome user ("name of user when he login")</p>
+      </div>
+    </>
+  );
 };
 export default Nav;
