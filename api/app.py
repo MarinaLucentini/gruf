1c644e96bee17088e8571a5e9f6773bb0c4e8977
judithukachukwu <ukachukwujudith95@gmail.com>
2024-12-15 06:36:56 +0100

    integrated the frontend with firebase

--- api/app.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-from views import app_views
-
-app = Flask(__name__)
-
-app.register_blueprint(app_views)
--- api/views/__init__.py
+++
@@ -1,4 +0,0 @@
-from flask import Blueprint
-app_views = Blueprint("app_views", __name__)
-
-from auth import *
